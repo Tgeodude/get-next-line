d55470c4e87815c0a9c3aef08dcc9f167410184d
Tojamura Geodude <tgeodude@vo-b1.msk.21-school.ru>
2021-12-02 16:08:37 +0300

    end

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgeodude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 14:52:08 by tgeodude          #+#    #+#             */
+/*   Updated: 2021/12/02 16:02:55 by tgeodude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char	*line_next_end(char **line_next, char **line)
+static char	*line_next_end(char **line_next, char **line)
 {
 	char	*pointer_n;
 	char	*clean;
@@ -12,10 +24,10 @@ char	*line_next_end(char **line_next, char **line)
 		if (pointer_n)
 		{
 			clean = *line_next;
-			*line = ft_strdup(*line_next);
 			*line_next = ft_strdup(++pointer_n);
-			free(clean);
 			*pointer_n = '\0';
+			*line = ft_strdup(clean);
+			free(clean);
 		}
 		else
 		{
@@ -28,27 +40,26 @@ char	*line_next_end(char **line_next, char **line)
 	return (pointer_n);
 }
 
-char	*get_next_line(int fd)
+static char	*free_line(char *line)
+{
+	free(line);
+	return (NULL);
+}
+
+static char	*line_start(int bf, int fd)
 {
 	char		buff[BUFFER_SIZE + 1];
-	char		*line;
 	char		*pointer_n;
+	char		*line;
 	static char	*line_next;
 	char		*clean;
-	int		bf;
 
-	if (BUFFER_SIZE <= 0 || fd < 0)
-		return(NULL);
-	bf = 1;
 	pointer_n = line_next_end(&line_next, &line);
 	while (!pointer_n && bf && !line_next)
 	{	
 		bf = read(fd, buff, BUFFER_SIZE);
-		if (bf <= 0  && *line == '\0')
-		{
-			free(line);
-			return (NULL);
-		}
+		if (bf <= 0 && *line == '\0')
+			return (free_line(line));
 		buff[bf] = '\0';
 		pointer_n = ft_strchr(buff, '\n');
 		if (pointer_n)
@@ -59,16 +70,13 @@ char	*get_next_line(int fd)
 		clean = line;
 		line = ft_strjoin(line, buff);
 		free(clean);
-
 	}
 	return (line);
 }
 
-/*int main (void)
+char	*get_next_line(int fd)
 {
-	int	fd;
-	fd = open("text.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-}*/
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	return (line_start(1, fd));
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgeodude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 16:06:50 by tgeodude          #+#    #+#             */
+/*   Updated: 2021/12/02 16:06:52 by tgeodude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -10,12 +22,11 @@
 #  define BUFFER_SIZE 20
 # endif
 
-
 void		*ft_memcpy(void *dest, const void *src, size_t len);
 char		*get_next_line(int fd);
 size_t		ft_strlen(const char *s);
 char		*ft_strdup(const char *str);
-char		*ft_strjoin(char const *s1,char const *s2);
+char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strchr(const char *string, int symbol);
 
 #endif
